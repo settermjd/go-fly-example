92593e81c28439410ce1a0f00423a2e8b1d5f217
Matthew Setter <matthew@matthewsetter.com>
2024-02-27 12:38:11 +1000

    Update the database provisioning script
    This is a reasonable increase to the script, attempting to do as much
    error handling as is possible to help ensure that nothing will go wrong
    during the execution of the script, potentially leading to a failed
    build.

--- bin/run-migrations.sh
+++ bin/run-migrations.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
-# Run the database migrations
-echo -e "Running database migrations with dbmate.\n"
-npx dbmate up
+set -u
+
+echo "Provisioning the database...."
+
+# Check if the database URL is both set *and* not empty.
+if [ -z "${DATABASE_URL:-}" ]; then
+    echo "Database URL is not available. Please set it. Exiting...."
+    exit 1
+fi
+
+# Extract the database directory from the DATABASE_URL environment variable
+db_url_prefix="sqlite:"
+db_dir=$(dirname ${DATABASE_URL#"$db_url_prefix"})
+
+# Create the database directory if it does not exist.
+if [ ! -d $db_dir ]; then
+    echo "Database directory [ ${db_dir} ] does not exist. Creating...."
+    mkdir -p "$db_dir"
+fi
+
+# Run any pending migrations
+# 1 is returned if there are pending migrations
+# 0 is returned if there are no pending migrations
+npx dbmate status --exit-code --quiet
+no_migrations=$?
+if [ ! $no_migrations ]; then
+    echo "Running pending migrations...."
+    npx dbmate up
+fi
+
+echo "....Finished provisioning the database."
+
+exit 0
